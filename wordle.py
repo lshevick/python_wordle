a2cbc0b75bec0533282769dc4765c40dbef596ef
Levi Shevick <levidshevick@gmail.com>
2022-06-08 16:19:04 -0400

    refactors functions for more simple functions

--- wordle.py
+++ wordle.py
@@ -1,6 +1,6 @@
 # create wordle in python
 # need to have a FIVE letter word that is displayed with blanks
-# users have six guesses, must dispaly each guess to show how many already guessed and how many remain
+# users have six guesses, must display each guess to show how many already guessed and how many remain
 # the users guess must be a valid word in list
 # when user makes a guess, it will update one of the blank guesses with their word
 # must show user what letters are in the word with yellow, and if they are not in word, grey
@@ -8,12 +8,13 @@
 
 #global variables
 
-guesses = 6
+guesses = 0
 word = 'plays'
 mystery = list(word)
 game_table = []
 guessed_words = []
 blanks = []
+submission = []
 
 def green_letter(l):
     return f'\033[1;30;42m {l} '
@@ -25,49 +26,74 @@ def grey_letter(l):
     return f'\033[1;30;47m {l} '
 
 
-def display_game(mystery):
-    global guesses, game_table, guessed_words, blanks
+def set_game_table(mystery):
+    global guesses, game_table, blanks
     for l in range(len(mystery)):
         blanks += ' _ '
-    for i in range(guesses):
+    for i in range(6):
         game_table.append(''.join(blanks)) 
-        print(game_table[i])
+        # print(game_table[i])
 
     return game_table
 
-
-
+def display_game():
+    global game_table
+    for n, i in enumerate(game_table):
+        print(game_table[n])
 
 def check_guess(mystery, guess):
-    global game_table, guessed_words
+    global submission, guessed_words
 
     for i, l in enumerate(guess):
         if guess[i] == mystery[i]:
-            guessed_words += green_letter(l)
+            submission += green_letter(l)
         elif l in mystery:
-            guessed_words += yellow_letter(l)
+            submission += yellow_letter(l)
         else:
-            guessed_words += grey_letter(l)
-    submission = ''.join(guessed_words)
-    return submission
+            submission += grey_letter(l)
+    submission_string = ''.join(submission) 
+    guessed_words.append(submission_string)
+    return guessed_words
 
-def update_table(submission):
-    pass
+# def add_sub(submision):
+#     guessed_words.append(submision)
+#     return guessed_words
 
+def update_table(guessed_words):
+    global game_table
 
-def play_game(mystery):
-    global game_table, guessed_words
-    
-    display_game(mystery)
+    for n, i in enumerate(game_table):
+        if n == guesses:
+            game_table[n] = guessed_words[n]
+        else:
+            continue
 
-    while guesses > 0:
+    return game_table
+
+def play_game():
+    global game_table, guessed_words, mystery, submission, guesses
+    set_game_table(mystery)
+
+    while guesses < 6:
+        display_game()
+
         guess = input('Try to guess the 5 letter word: ')
+        
+        if len(guess) != 5 or any(char.isdigit() for char in guess) == True:
+            print('You need to guess a FIVE letter WORD')
+            continue
+        else:
+            check_guess(mystery, guess)
+            # add_sub(submission)
+            update_table(guessed_words)
+            guesses += 1
 
 
-    
 
-display_game(mystery)
-# check_guess(mystery, 'glass')
-print(game_table[0])
+play_game()  
 
-print(check_guess(mystery, 'glass'))
+# display_game(mystery)
+# # check_guess(mystery, 'glass')
+# print(game_table[0])
+
+# print(check_guess(mystery, 'glass'))
